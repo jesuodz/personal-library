b6c5c9f19c9aa845787b3ac327dff755356801d0
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-08-20 13:34:45 -0400

    save progress

--- controllers/getAllBooks.js
+++ controllers/getAllBooks.js
@@ -2,14 +2,23 @@ const Book = require('../models/Book');
 
 module.exports = (req, res) => {
   
-  Book.find({}).then(books => {
-    const booksCommentCount = books.map(book => {
-      return {
-        ...book._doc,
-        commentCount: book.comments.length
+  Book.
+    aggregate([
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          commentCount: {
+            count: '$comments'
+          }
+        }
       }
-    });
+    ]).
+    then(books => res.json(books)).
+    catch(err => res.json(err));
 
-    res.json(booksCommentCount);
-  });
+  // Book.aggregate([
+  //   { $match: {} },
+  //   { $group: { commentCount: { $get: "" } }
+  // ])
 }
