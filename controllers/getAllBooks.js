634a795cdc0f446c6891350da5bfd36ed42fc826
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-08-20 14:06:09 -0400

    Fix aggregation

--- controllers/getAllBooks.js
+++ controllers/getAllBooks.js
@@ -2,23 +2,15 @@ const Book = require('../models/Book');
 
 module.exports = (req, res) => {
   
-  Book.
-    aggregate([
-      {
-        $project: {
-          _id: 1,
-          title: 1,
-          commentCount: {
-            count: '$comments'
-          }
-        }
-      }
-    ]).
-    then(books => res.json(books)).
-    catch(err => res.json(err));
+  const projection = {
+    _id: 1,
+    title: 1,
+    commentCount: {
+      $size: '$comments'
+    }
+  }
 
-  // Book.aggregate([
-  //   { $match: {} },
-  //   { $group: { commentCount: { $get: "" } }
-  // ])
+  Book.aggregate([{ $project : projection }])
+    .then(books => res.json(books))
+    .catch(err => res.json(err));
 }
