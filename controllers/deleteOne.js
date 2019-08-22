d4f898224e5f5695a7ea439d0ff03683cbf6f255
Jesus Ordosgoitty <jesuodz@gmail.com>
2019-08-22 08:58:26 -0400

    Refactor

--- controllers/deleteOne.js
+++ controllers/deleteOne.js
@@ -4,8 +4,6 @@ module.exports = (req, res) => {
 
   Book.
     findOneAndDelete({ _id: req.params.id }).
-    then(book => {
-      res.json({ success : book._id })
-    }).
+    then(book => res.json({ success : book._id })).
     catch(err => res.status(404).json({ 'notfound': 'book no exists' }));
 }
